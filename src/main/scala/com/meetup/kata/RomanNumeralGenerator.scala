0b04de1c48320c3e71ff286e66193100b2ffa0c6
Jordi Pradel <jordi.pradel@agilogy.com>
2013-04-02 20:58:58 +0200

    Ouch

--- src/main/scala/com/meetup/kata/RomanNumeralGenerator.scala
+++ src/main/scala/com/meetup/kata/RomanNumeralGenerator.scala
@@ -5,18 +5,11 @@ import annotation.tailrec
 object RomanNumeralGenerator {
   val coolSymbols = List(1000 ->"M" , 100->"C" , 10 -> "X", 1 -> "I")
   val uncoolSymbols = List(500 -> "D" , 50-> "L" ,  5 -> "V")
-  val symbols = // List(50-> "L", 40-> "XL", 10 -> "X", 9 -> "IX", 5 -> "V", 4 -> "IV", 1 -> "I")
-  {
-    val oddSymbols = {coolSymbols ::: uncoolSymbols} flatMap {
+  val symbols = {(coolSymbols ::: uncoolSymbols) flatMap {
       s =>
-        val cool = coolSymbols.filter(cs => cs._1 < s._1).headOption
-        cool.map(c => (s._1 - c._1) -> (c._2 + s._2))
-    }
-    (coolSymbols ::: uncoolSymbols ::: oddSymbols).sortBy(_._1).reverse
-  }
-  //val deducedSymbols = List(900 -> "CM" , 400 -> "CD" , 90 -> "XC" , 40 -> "XL" , 9 -> "IX", 4 -> "IV")
-
-
+        Seq(s) ++ coolSymbols.filter(cs => cs._1 < s._1)
+          .headOption.map(c => (s._1 - c._1) -> (c._2 + s._2)).toSeq
+    }}.sortBy(- _._1)
 
   def generate(i: Int): String = {
 
